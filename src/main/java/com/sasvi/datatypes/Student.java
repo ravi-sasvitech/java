a437240f167c78be3a9005074bd56ce51575452f
ravin <ravin@Ravi.attlocal.net>
2023-01-10 12:17:38 -0600

    Created Student class

---
+++ src/main/java/com/sasvi/datatypes/Student.java
@@ -0,0 +1,25 @@
+package com.sasvi.datatypes;
+
+public class Student {
+	private int number;
+	private String name;
+	private String branch;
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+}
