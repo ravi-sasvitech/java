3e571a13f9b1f5bb5ecba17d1fda61e4a18d3a0d
kavit <kavit@LAPTOP-GJGHQVI5.attlocal.net>
2023-03-28 11:52:01 -0400

    - added set program

--- src/main/java/com/sasvi/collections/kavitha/StudentsSetProgram.java
+++ src/main/java/com/sasvi/collections/kavitha/StudentsSetProgram.java
@@ -1,6 +1,11 @@
 package com.sasvi.collections.kavitha;
 
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import com.sasvi.datatypes.Student;
@@ -8,20 +13,31 @@ import com.sasvi.datatypes.Student;
 public class StudentsSetProgram {
 	
 	public static void main(String args[]) {
-		createStudentsHashSet();
+		
+		StudentsSetProgram studentSetProgram = new StudentsSetProgram();
+		Set<Student> studentsSet = studentSetProgram.createStudentsHashSet();
+		System.out.println("Students info: ");
+		
+		studentSetProgram.printStudnetsInfo(studentsSet);
+		
+//		studentSetProgram.filterEceStudents(studentsSet);
+		
+		//sortStudentsByBranch(studentsSet);
+		
+		
 	}
-	
-	
+
 	/**
-	 * This method creates list with student objects 
-	 * @return list
+	 * This method creates set with student objects 
+	 * @return set
 	 */
-	public static Set<Student> createStudentsHashSet() {
+	public Set<Student> createStudentsHashSet() {
 		
 		Set<Student> students = new HashSet<Student>();
 		
-		Student kavitha = new Student(1, "kavitha", "cse");
-		students.add(kavitha);
+		students.add(new Student(1, "kavitha", "cse"));
+		
+		students.add(new Student(1, "kavitha", "cse"));
 		
 		students.add(new Student(2, "ravi", "cse"));
 		
@@ -32,14 +48,45 @@ public class StudentsSetProgram {
 		return students;
 	}
 	
-	/**
-	 * This method creates list with student objects 
-	 * @return list
-	 */
-	public static Set<Student> createStudentsLinkedHashSet() {
-		return null;
-
+	public void printStudnetsInfo(Set<Student> students) {
+		
+		// Printing students info using for each loop
+				for(Student student : students) {
+					System.out.println(student);
+			     }
 	}
 	
+	/**
+	 * Takes students collection as input, creates a new set with ece students and returns it.
+	 * 
+	 * @param students
+	 * @return
+	 */
+
+	public Set<Student> filterEceStudents(Set<Student> students) {
+		//filter ece students
+		
+		Set<Student> eceStudents = new  HashSet<Student>();
+		
+		for(Student student : students) {
+			if(student.getBranch().equals("ece")) {
+				eceStudents.add(student);
+			}
+		}
+		return eceStudents;
+	}
+	
+	
+//	private Set<Student> sortStudentsByBranch(Set<Student> studentsSet) {
+//		
+//		List<Student> studentList = new ArrayList<>(studentsSet);
+//		
+//		Comparator<Student> departmentComparator = Comparator.comparing(Student::getBranch);
+//		
+//		
+//		return null;
+//		// TODO Auto-generated method stub
+//	
+//	}
 
 }
